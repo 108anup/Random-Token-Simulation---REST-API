168370afc69920a5064bfce4fc49cfd6c6b6e401
Anup Agarwal <108anup@gmail.com>
2016-01-24 11:42:45 +0530

    Optimized Code and Improved Linguistics and Grammar

--- index.php
+++ index.php
@@ -4,54 +4,42 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.css">
-	<script source="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/styles.css">
-	<meta charset="UTF-8">
 
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
-	
-	<title>Random Token Simulation - REST API</title>
-</head>
-<body>
 <?php
 	$display = true;
 
 	if(isset($_GET['num_dice'])){
+		$num_faces=6;
+
+		if(isset($_GET['num_faces']))
+			$num_faces=$_GET['num_faces'];
+
 		for ($i=0; $i < $_GET['num_dice']; $i++) { 
-			$data_array["Dice #".($i+1)] = mt_rand(1,6);
+			$data_array["Dice #".($i+1)] = mt_rand(1,$num_faces);
 		}
 		$display=false;
 		echo json_encode($data_array);
 	}
 
-	else if(isset($_GET['num_coin']) && !isset($_GET['bias_percent']) && !isset($_GET['bias_winner'])){
-		for ($i=0; $i < $_GET['num_coin']; $i++) { 
-			if(mt_rand(1,2)==2)
-				$data_array["Coin #".($i+1)] = 'H';
-			else
-				$data_array["Coin #".($i+1)] = 'T';
-		}
-		$display=false;
-		echo json_encode($data_array);
-	}
+	else if(isset($_GET['num_coin'])){
+		$winner = 'H';
+		$percent = 50;
 
-	else if(isset($_GET['num_coin']) && isset($_GET['bias_percent']) && isset($_GET['bias_winner'])){
+		if(isset($_GET['bias_winner']))
+			$winner=$_GET['bias_winner'];
+		if(isset($_GET['bias_percent']))
+			$percent = $_GET['bias_percent'];
 		
-		if($_GET['bias_winner']==='H'){
+		if($winner==='H'){
 			$loser='T';
 		}
 		else{
 			$loser='H';
 		}
 
-
 		for ($i=0; $i < $_GET['num_coin']; $i++) { 
-			if(mt_rand(1,100)<=$_GET['bias_percent'])
-				$data_array["Coin #".($i+1)] = $_GET['bias_winner'];
+			if(mt_rand(1,100)<=$percent)
+				$data_array["Coin #".($i+1)] = $winner;
 			else
 				$data_array["Coin #".($i+1)] = $loser;
 		}
@@ -63,9 +51,3 @@
 		include 'mainbody.php';
 	}
 ?>
-
-	
-	
-	
-</body>
-</html>
--- mainbody.php
+++ mainbody.php
@@ -1,27 +1,46 @@
-<div class="jumbotron text-center">
-		<h1 class="title">Random Token Simulation</h1>
-		<h3 class="subtitle">An API</h3>
-	</div>
-	
-	<div class="usage">
-		<h2 class="text-center">Simulate A Coin</h2>
-			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2"><h3>Toss 'n' Coins (<var>num_coin</var>)</h3></div>
-			</div>
-			<div class="row">
-				<pre class="col-sm-offset-2 col-sm-8">index.php?num_coin=100</pre>
-			</div>
-			<div class="row">
-				<div class="col-sm-offset-2 extras">
-					By Default the coin is unbiased and there is 50% chance that a Head will occur.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.css">
+	<script source="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/styles.css">
+	<meta charset="UTF-8">
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
+
+	<title>Random Token Simulation - REST API</title>
+</head>
+<body>
+	<div class="jumbotron text-center">
+			<h1 class="title">Random Token Simulation</h1>
+			<h3 class="subtitle">An API</h3>
+		</div>
+		
+		<div class="usage">
+
+			<h2 class="text-center">Simulate A Coin</h2>
+
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2"><h3>Toss 'n' Coins (<var>num_coin</var>)</h3></div>
 				</div>
-			</div>
-			<br>
-			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2"><h3 class="response">Response:</h3></div>
-			</div>
-			<div class="row">
-				<pre class="col-sm-offset-2 col-sm-8">{
+
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">index.php?num_coin=10</pre>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-offset-2 extras">
+						By Default the coin is unbiased and there is 50% chance that a Head will occur.
+					</div>
+				</div>
+
+				<br>
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2"><h3 class="response">Response:</h3></div>
+				</div>
+
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">{
 	"Coin #1":"T",
 	"Coin #2":"H",
 	"Coin #3":"T",
@@ -33,23 +52,27 @@
 	"Coin #9":"T",
 	"Coin #10":"T"
 }</pre>
-			</div>
-			<br>
-			<br>
-			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2"><h3>Bias That Coin (<var>bias_percent, bias_winner</var>)</h3></div>
-			</div>
-			<div class="row">
-				<pre class="col-sm-offset-2 col-sm-8">index.php?num_coin=10&bias_percent=80&bias_winner=H</pre>
-			</div>
-			<div class="row">
-				<div class="col-sm-offset-2 extras">
-					There is 80% chance on every toss that a Head will occur, this does not ensure that in 100 trials exactly 80 Heads will occur.
 				</div>
-			</div>
-			<br>
-			<div class="row">
-				<pre class="col-sm-offset-2 col-sm-8">{
+
+				<br>
+				<br>
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2"><h3>Bias That Coin (<var>bias_percent, bias_winner</var>)</h3></div>
+				</div>
+
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">index.php?num_coin=10&bias_percent=80&bias_winner=H</pre>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-offset-2 extras">
+						For every toss there is 80% chance that a Head will occur, this does not ensure that in 100 trials exactly 80 Heads will occur.
+					</div>
+				</div>
+
+				<br>
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">{
 	"Coin #1":"H",
 	"Coin #2":"T",
 	"Coin #3":"H",
@@ -61,4 +84,76 @@
 	"Coin #9":"H",
 	"Coin #10":"H"
 }</pre>
-	</div>
+				</div>	
+
+		<br><br><br>
+		<h2 class="text-center">Simulate A Dice</h2>
+
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2"><h3>Roll 'n' Die (<var>num_dice</var>)</h3></div>
+				</div>
+
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">index.php?num_dice=10</pre>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-offset-2 extras">
+						By Default The dice will have 6 faces.
+					</div>
+				</div>
+
+				<br>
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2"><h3 class="response">Response:</h3></div>
+				</div>
+
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">{
+	"Dice #1":3,
+	"Dice #2":5,
+	"Dice #3":1,
+	"Dice #4":1,
+	"Dice #5":4,
+	"Dice #6":1,
+	"Dice #7":3,
+	"Dice #8":1,
+	"Dice #9":6,
+	"Dice #10":2
+}</pre>
+				</div>
+
+				<br>
+				<br>
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2"><h3>Change <var>num_faces</var></h3></div>
+				</div>
+
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">index.php?num_coin=10&num_faces=4</pre>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-offset-2 extras">
+						For 4 faces, A Tetrahedral Dice would be assumed.
+					</div>
+				</div>
+
+				<br>
+				<div class="row">
+					<pre class="col-sm-offset-2 col-sm-8">{
+	"Dice #1":4,
+	"Dice #2":2,
+	"Dice #3":2,
+	"Dice #4":1,
+	"Dice #5":3,
+	"Dice #6":4,
+	"Dice #7":4,
+	"Dice #8":1,
+	"Dice #9":4,
+	"Dice #10":2
+}</pre>
+				</div>
+		</div>
+</body>
+</html>
